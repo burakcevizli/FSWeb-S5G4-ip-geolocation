c7f2f399623f6dc321656c5e0959a76b6c0291a8
KrieqqeR <burakcevizli@gmail.com>
2023-07-27 23:31:29 +0300

    Ödev ypaıldı ancak undefined diyor

--- ip/index.js
+++ ip/index.js
@@ -1,4 +1,4 @@
-//axios import buraya gelecek
+import axios from 'axios';
 
 var benimIP;
 
@@ -25,11 +25,19 @@ async function ipAdresimiAl(){
 /*
 	ADIM 1: axios kullanarak, aşağıdaki URL'ye GET sorgusu atacağız
     (tag içindeki yere kendi ipnizi yazarak URL'yi oluşturun):
-    https://apis.ergineer.com/ipgeoapi/<ipniz>
+    https://apis.ergineer.com/ipgeoapi/94.55.132.38
 	
 	NOT: Bilgisayarın IP adresini öğrenmek için: https://apis.ergineer.com/ipadresim 
 	ADIM 5'e gelene kadar fonksiyonunuzu test etmek için ip nizi URL'ye manuel olarak ekleyebilirsiniz.
 */
+
+
+let veriler = axios.get("https://apis.ergineer.com/ipgeoapi/94.55.132.38").then((response)=>{
+	return response.data
+})
+
+
+
 
 /*
 	ADIM 2: Geri döndürülen verileri inceleyin, bu sizin ip bilgileriniz! Bileşen fonksiyonunuzu geliştirmek içindeki bu veri yapısını
@@ -54,10 +62,49 @@ async function ipAdresimiAl(){
     </div>
 */
 
+	const nesneliFonksiyon = ((nesne=>{
+		const cardDiv = document.createElement("div")
+		cardDiv.setAttribute("class","card")
+		const flagImg = document.createElement("img")
+		flagImg.setAttribute("src","https://flagsapi.com/TR/shiny/64.png")
+		const cardInfoDiv = document.createElement("div")
+		cardInfoDiv.setAttribute("class","card-info")
+		const h3 = document.createElement("h3")
+		h3.setAttribute("class","ip")
+		h3.textContent=nesne["boylam"]
+		const pUlke = document.createElement("p")
+		pUlke.setAttribute("class","ulke")
+		pUlke.textContent = nesne["ülke"]+"("+nesne["ülkeKodu"]+")"
+		const pEnlem = document.createElement("p")
+		pEnlem.textContent="Enlem: "+ nesne["enlem"]+" Boylam: " + nesne["Boylam"]
+		const pSehir = document.createElement("p")
+		pSehir.textContent = "Sehir: " +nesne["şehir"]
+		const pSaat = document.createElement("p")
+		pSaat.textContent = "Saat Dilimi: "+nesne["saatdilimi"]
+		const pPara = document.createElement("p")
+		pPara.textContent = "Para Birimi: "+nesne["parabirimi"]
+		const pIsp = document.createElement("p")
+		pIsp.textContent = "ISP : "+nesne["isp"]
+
+		cardInfoDiv.append(h3,pUlke,pEnlem,pSehir,pSaat,pPara,pIsp)
+		cardDiv.append(flagImg)
+		cardDiv.append(cardInfoDiv)
+
+		return cardDiv;
+
+	}))
+
+
 /*
 	ADIM 4: API'den alınan verileri kullanarak ADIM 3'te verilen yapıda bir kart oluşturun ve 
 	bu kartı DOM olarak .cards elementinin içine ekleyin. 
 */
+
+	
+
+	const cards = document.querySelector(".cards").append(nesneliFonksiyon(veriler))
+
+	
 
 /*
 	ADIM 5: Manuel olarak eklediğiniz IP adresini dinamiğe dönüştürün. 
@@ -70,3 +117,5 @@ async function ipAdresimiAl(){
 
 
 //kodlar buraya gelecek
+
+
